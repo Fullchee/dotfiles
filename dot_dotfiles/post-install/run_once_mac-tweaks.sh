a9fb652b9ad56662b850c895c21c114f55874bf7
Fullchee Zhang <11246258+Fullchee@users.noreply.github.com>
2026-02-22 21:31:24 -0500

    Add back all the mac system tweaks from the original post-install script

--- dot_dotfiles/post-install/run_once_mac-tweaks.sh
+++ dot_dotfiles/post-install/run_once_mac-tweaks.sh
@@ -2,45 +2,105 @@
 
 # TODO: ask LLM to vet these and remove the ones that are outdated
 
+### Calendar
 defaults write com.apple.iCal "Default duration in minutes for new event" -int 30
 killall Calendar
+
+### DOCK
+# Speed up dock show/hide
 defaults write com.apple.dock autohide-delay -float 0
 defaults write com.apple.dock autohide-time-modifier -int 0
+# Dock to left
 defaults write com.apple.dock orientation -string "left"
-defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="spacer-tile";}'
-defaults write com.apple.dock showhidden -bool TRUE
-defaults write com.apple.dock tilesize -int 36
-defaults write com.apple.dock persistent-apps -array
+defaults write com.apple.dock persistent-apps -array-add '{"tile-type"="spacer-tile";}';
+defaults write com.apple.dock showhidden -bool TRUE;
+defaults write com.apple.dock tilesize -int 36;
+defaults write com.apple.dock persistent-apps -array;  # remove all the default permanent apps on the dock
+
+# Add Chrome to Dock
 defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Google Chrome.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
-# ... (remaining defaults commands shortened for brevity)
+# Add iTerm2 to Dock
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/iTerm.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+# Add Obsidian to Dock
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Obsidian.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+# Add VSCode to Dock
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Visual Studio Code.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+# Add Session to Dock
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Session.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+
+
+# spacer tiles in the dock
+defaults write com.apple.dock persistent-apps -array-add '{tile-data={}; tile-type="spacer-tile";}'
+# Add Messenger to Dock
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Messenger.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+# Add WeChat to Dock
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/WeChat.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+# Add WhatsApp to Dock
+defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/WhatsApp.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+# spacer tiles in the dock
+defaults write com.apple.dock persistent-apps -array-add '{tile-data={}; tile-type="spacer-tile";}'
+defaults write com.apple.dock wvous-br-corner -int 0  # remove hot corner to quickly create new note
 killall Dock
+
+### FINDER
+# Path at the top of finder
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
-defaults write com.apple.finder AppleShowAllFiles YES
-defaults write com.apple.finder ShowPathbar -bool true
-killall Finder
+# show dotfiles in Finder
+defaults write com.apple.finder AppleShowAllFiles YES;
+defaults write com.apple.finder ShowPathbar -bool true  # doesn't seem to work
+killall Finder;
+
+
+### Keyboard shortcuts
+
+### Menu bar
+# show bluetooth in menu bar
 /usr/bin/defaults write com.apple.controlcenter.plist Bluetooth -int 18
 defaults write ~/Library/Preferences/ByHost/com.apple.controlcenter.plist BatteryShowPercentage -bool true
 killall ControlCenter
+
+
+### Night Shift
 defaults write com.apple.CoreBrightness CBBlueReductionSchedule -dict \
-enabled -bool true \
-start -float 20.0 \
-end -float 6.0;
+  enabled -bool true \
+  start -float 20.0 \
+  end -float 6.0;
 killall CoreBrightness
+
+
+### SAFARI
+# Show the full website address in Safari's address bar
 defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
+# Enable Safari's Develop menu and Web Inspector
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
 killall Safari
+
+### Screenshot
 defaults write com.apple.screencapture type clipboard
 killall SystemUIServer
+
+### TRACKPAD
+# increase trackpad sensitivity
 defaults write -g com.apple.mouse.scaling 4.0
 defaults write -g com.apple.trackpad.scaling 4.0
+# Disable swipe between pages
 defaults write -g AppleEnableSwipeNavigateWithScrolls -bool false
 defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerHorizSwipeGesture -int 0
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerHorizSwipeGesture -int 0
+# App Exposé: swipe down with three fingers
 defaults write com.apple.dock showAppExposeGestureEnabled -bool true
 defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerVertSwipeGesture -int 2
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerVertSwipeGesture -int 2
+# feedback sound when changing volume
 defaults write -g com.apple.sound.beep.feedback -integer 1
+
+
+# Not sure if this works
 defaults write NSGlobalDomain AppleShowScrollBars -string Always
-sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
+
+
+# not sure if I need the following
+# Tell system when Xcode utilities live:
+sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer;
