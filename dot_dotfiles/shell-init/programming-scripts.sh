2f35e6dd23d90b703246f58f1fa6c7d6e3932092
Fullchee Zhang <11246258+Fullchee@users.noreply.github.com>
2026-02-26 09:19:30 -0500

    perf: remove duplicate compinit, move docker fpath before prezto, drop redundant node module

--- dot_dotfiles/shell-init/programming-scripts.sh
+++ dot_dotfiles/shell-init/programming-scripts.sh
@@ -4,13 +4,6 @@
 # Databases
 # Networking
 
-## -----
-# The following lines have been added by Docker Desktop to enable Docker CLI completions.
-fpath=($HOME/.docker/completions $fpath)
-autoload -Uz compinit
-compinit
-# End of Docker CLI completions
-
 ## -------- General --------
 alias sz="source ~/.zshrc"
 eval "$(zoxide init zsh)"
