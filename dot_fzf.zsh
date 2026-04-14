0dc0c4a0a175f0a9c84aee2e17c11862b3457c7f
Fullchee Zhang <11246258+Fullchee@users.noreply.github.com>
2026-04-14 19:21:32 -0400

    Try to fix hanging terminal

--- dot_fzf.zsh
+++ dot_fzf.zsh
@@ -1,7 +1,19 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+FZF_BASE=""
+if [[ -d /opt/homebrew/opt/fzf ]]; then
+  FZF_BASE=/opt/homebrew/opt/fzf
+elif [[ -d /usr/local/opt/fzf ]]; then
+  FZF_BASE=/usr/local/opt/fzf
 fi
 
-source <(fzf --zsh)
+if [[ -n "$FZF_BASE" && ! "$PATH" == *$FZF_BASE/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/$FZF_BASE/bin"
+fi
+
+if [[ -n "$FZF_BASE" && -f "$FZF_BASE/shell/completion.zsh" && -f "$FZF_BASE/shell/key-bindings.zsh" ]]; then
+  source "$FZF_BASE/shell/completion.zsh"
+  source "$FZF_BASE/shell/key-bindings.zsh"
+else
+  source <(fzf --zsh)
+fi
